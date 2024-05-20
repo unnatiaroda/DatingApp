9c4e8e0962dbd195d01378af0e74ae5c221ee709
unnatiaroda <aroraunnati1510@gmail.com>
2024-05-20 18:34:02 +0530

    Client Login and Register

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();  //because we want to emit something from child to parent component
+  model: any = {}
+
+  constructor(private accountService: AccountService) { }
+  
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: () => {
+        this.cancel();
+      },
+      error: error => console.log(error)
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
