14904fe4923ef9c68ff148357e4b8464b34404f6
unnatiaroda <aroraunnati1510@gmail.com>
2024-05-20 11:42:20 +0530

    Added Authentication using JWT

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,41 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService : ITokenService
+{
+    private readonly SymmetricSecurityKey _key;
+    public TokenService(IConfiguration config)
+    {
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])); 
+        // "TokenKey" value should be atleast 12 characters
+        // In real world scenario, it is atleast 64 characters long
+    }
+    public string CreateToken(AppUser user)
+    {
+        var claims = new List<Claim>()
+        {
+            new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+        };
+
+        var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature); //HmacSha512Signature: strongest available in that list
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.Now.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
