e6933185da7254237b564ee15a261c17970ca66e
unnatiaroda <aroraunnati1510@gmail.com>
2024-05-23 14:49:00 +0530

    Building user interface

---
+++ client/src/app/members/member-cards/member-cards.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+
+@Component({
+  selector: 'app-member-cards',
+  templateUrl: './member-cards.component.html',
+  styleUrls: ['./member-cards.component.css']
+})
+export class MemberCardsComponent implements OnInit {
+
+  @Input() member: Member | undefined
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
